d327f65b12fcf7ed64513eab1964c6492af4d802
WaGjUb <danielcostavalerio@gmail.com>
2017-10-29 15:58:19 -0200

    instalação para as bibliotecas

--- run.sh
+++ run.sh
@@ -1,20 +1,31 @@
 #!/bin/sh
 
 PYTHON_BIN=./environment/bin/python
+PIP_BIN=./environment/bin/pip
+REQUERIMENTS="./requeriments.txt"
 TEST_LIB_FILE="./test/orb_teste.py"
 ERROR=false
 
-readonly PYTHON_BIN TEST_LIB_FILE
+readonly PYTHON_BIN TEST_LIB_FILE REQUERIMENTS
 
-# test the libs of the project
+## Run a code of test 
 test_lib()
 {
+
 	$PYTHON_BIN $TEST_LIB_FILE
+}
+
+## Install the libs contained in requeriments
+install_libs()
+{
+	$PIP_BIN install -r $REQUERIMENTS
 }
 
 if [ -z $1 ]; then
 	echo "Nenhum argumento foi passado" 
 	ERROR=true
+elif [ $1 == "init" ]; then
+	install_libs
 elif [ $1 == "teste" ]; then
 	test_lib
 elif [ $1 == "python" ]; then
@@ -26,6 +37,7 @@ fi
 
 if $ERROR ; then
 	echo ""
+	echo "init: instala as bibliotecas"
 	echo "'teste': executa o código exemplo"
 	echo "'python': abre o interpretador do ambiente"
 fi
